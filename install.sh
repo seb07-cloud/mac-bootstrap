4d557616a1b17917bf3c6d51d69cd062b43b2d2d
seb07-cloud <wild.sebastian@outlook.de>
2023-05-31 18:23:54 +0200

    initial

--- install.sh
+++ install.sh
@@ -1,70 +1,5 @@
 #!/bin/bash
 
-apps=(
-    AudioSwitcher
-    Alfred
-    1Password 
-    AltTab
-    AppCleaner
-    Bartender
-    BetterTouchTool
-    Caffeine
-    BetterDisplay
-    BetterSnapTool
-    BetterTouchTool
-    CheatSheet
-    Black Magic Disk Speed Test
-    Captain
-    Close All
-    Command X
-    Docker
-    Discord
-    draw.io
-    Firefox
-    ForkLift
-    Git
-    Foxit
-    Outlook
-    iTerm 
-    Stats
-    Teams
-    Visual Studio Code
-    GreenShot
-    Homeassistant
-    HyperDuck
-    Karabiner Elements
-    Ledger Live
-    Logi Options
-    Menuwhere
-    Microsoft Edge
-    Microsoft Remote Desktop
-    Microsoft Teams
-    Microsoft Excel 
-    Microsoft Word
-    Microsoft Powerpoint
-    Microsoft OneNote
-    Microsoft OneDrive
-    Microsoft Outlook
-    Mini Play
-    Obsidian
-    Postman
-    Powershell
-    SensibleSideButtons
-    Sonos
-    Spotify
-    Steam
-    Stream Deck
-    Telegram 
-    Twitter
-    TinkerTool
-    The Unarchiver
-    TradingView
-    UTM 
-    Zed
-)
-
-echo "Installing apps with Homebrew..."
-
 # Check if Homebrew is installed, install if not
 if test ! $(which brew); then
   echo "Installing Homebrew..."
@@ -77,16 +12,60 @@ brew update
 echo "Installing Git..."
 brew install git
 
+declare -a StringArray=(
+  "alfred" 
+  "1password"
+  "alt-tab"
+  "appcleaner"
+  "bartender"
+  "bettertouchtool"
+  "caffeine"
+  "bettersnaptool"
+  "cheatsheet"
+  "blackmagic-disk-speed-test"
+  "captain"
+  "docker"
+  "discord"
+  "drawio"
+  "firefox"
+  "forklift"
+  "git"
+  "foxit-reader"
+  "microsoft-outlook"
+  "iterm2"
+  "stats"
+  "microsoft-teams"
+  "visual-studio-code"
+  "greenshot"
+  "homeassistant"
+  "karabiner-elements"
+  "ledger-live"
+  "logitech-options"
+  "menuwhere"
+  "microsoft-edge"
+  "microsoft-remote-desktop"
+  "microsoft-office" # This will include Excel, Word, Powerpoint, OneNote
+  "microsoft-onedrive"
+  "obsidian"
+  "postman"
+  "powershell"
+  "sensiblesidebuttons"
+  "sonos"
+  "spotify"
+  "steam"
+  "telegram"
+  "twitter"
+  "tinkertool"
+  "the-unarchiver"
+  "tradingview"
+  "utm"
+  "zed"
+)
+
 # Iterate the string array using for loop
-
-for val in ${apps[@]}; do
+for val in ${StringArray[@]}; do
    echo "Installing $val..."
-   if test ! $(which $val); then
-    echo "Installing $val..."
-    brew install --cask $val
-   fi
-done
-
+   brew install --cask $val
 done
 
 echo "Done installing!"
